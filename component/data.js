657806e5f9cf22ae0a670a98b54306b762f6fbd6
KevinsienHeldrayetno <103363748+KevinsienHeldrayetno@users.noreply.github.com>
2022-10-22 22:19:27 +0700

    Add files via upload

---
+++ component/data.js
@@ -0,0 +1,37 @@
+const DataPenerbangan = [
+    [
+      "Lampung",
+      "Aceh",
+      "Jakarta",
+      "Bandung",
+      "Kalimantan",
+    ],
+    [
+      "Lampung",
+      "Aceh",
+      "Jakarta",
+      "Bandung",
+      "Kalimantan",
+    ],
+    [
+      "09 September 2022",
+      "10 September 2022",
+      "11 September 2022",
+      "12 September 2022",
+      "13 September 2022",
+    ],
+    [
+      "Elang",
+      "Lion Air",
+      "Majapahit",
+      "Elang",
+      "Lion Air",
+      "Majapahit",
+      "Majapahit",
+      "Elang",
+      "Lion Air",
+      "Majapahit",
+    ],
+  ];
+  
+  export default DataPenerbangan;
